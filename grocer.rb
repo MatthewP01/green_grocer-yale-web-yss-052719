f6c88e4251ef50c4545e3c79c23c6f460c43c903
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 21:59:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
   coupon_hash = {}
   cart.each do |x, y|
     coupons.each do |coupon|
-      if x == coupon[:item] && info[:count] >= coupon[:num]
+      if x == coupon[:item] && y[:count] >= coupon[:num]
         cart[x][:count] = cart[x][:count] - coupon[:num]
         if coupon_hash[x + " W/COUPON"]
           coupon_hash[x + " W/COUPON"][:count] += 1
