a068827774deb394ca46e6c82fe0ec381a10b6c2
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 21:54:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,25 +36,25 @@ def apply_coupons(cart, coupons)
   end
   return coupon_hash
 end
-def apply_coupons(cart, coupons)
-  # code here
-  new_cart = {}
-  cart.each do |grocery, info|
-    coupons.each do |coupon|
-      if grocery == coupon[:item] && info[:count] >= coupon[:num]
-        cart[grocery][:count] = cart[grocery][:count] - coupon[:num]
-        if new_cart[grocery + " W/COUPON"]
-          new_cart[grocery + " W/COUPON"][:count] += 1
-        else
-          new_cart[grocery + " W/COUPON"] = {:price => coupon[:cost], :clearance => cart[grocery][:clearance], :count => 1}
-        end
-      end
-    end
-    new_cart[grocery] = info
-
-  end
-  new_cart
-end
+# def apply_coupons(cart, coupons)
+#   # code here
+#   new_cart = {}
+#   cart.each do |grocery, info|
+#     coupons.each do |coupon|
+#       if grocery == coupon[:item] && info[:count] >= coupon[:num]
+#         cart[grocery][:count] = cart[grocery][:count] - coupon[:num]
+#         if new_cart[grocery + " W/COUPON"]
+#           new_cart[grocery + " W/COUPON"][:count] += 1
+#         else
+#           new_cart[grocery + " W/COUPON"] = {:price => coupon[:cost], :clearance => cart[grocery][:clearance], :count => 1}
+#         end
+#       end
+#     end
+#     new_cart[grocery] = info
+#
+#   end
+#   new_cart
+# end
 
 def apply_clearance(cart)
   # code here
