a074c60112db9e8543047e13db70b92ad37ea198
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 21:48:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,6 +28,8 @@ def apply_coupons(cart, coupons)
           cart[x][:count] = cart[x][:count] - i[:num]
           if coupon_hash.include?(x + "W/COUPON")
             coupon_hash[x + "W/COUPON"][:count] += 1
+          else
+            
           end
         end
 
