23358715bcf0ad934e52de3382ae8dac5ef528a1
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 22:12:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -67,7 +67,6 @@ end
 
 def checkout(cart, coupons)
   check_out = consolidate_cart(cart)
-  
-  check_out = apply_coupons(cart: cart, coupons: coupons)
+  check_out = apply_coupons(cart: [cart], coupons: [coupons])
   cacheck_outrt = apply_clearance(cart: cart)
 end
